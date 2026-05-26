9d3312edafb2514b890240756364898e98e0d096
Alan <211577897+alancodecmd@users.noreply.github.com>
2026-05-26 16:31:40 -0300

    Finish all sections. Only proper styling left

--- js/script.js
+++ js/script.js
@@ -23,7 +23,7 @@ var swiper = new Swiper(".slide-content", {
           slidesPerView: 2,
         },
         950: {
-          slidesPerView: 2,
+          slidesPerView: 3,
         },
       },
     });
